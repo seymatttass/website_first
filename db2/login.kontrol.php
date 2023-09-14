8442a25f876b2512ee3565b105585307b6491aa2
Şeyma Taş <126778789+seymatttass@users.noreply.github.com>
2023-09-14 13:37:59 +0300

    Delete db2 directory

--- db2/login.kontrol.php
+++
@@ -1,9 +0,0 @@
-<?php
-@session_start();
-if (isset($_SESSION['girisyapti'])) {
-  // Oturum açmış
-} else {
-  // Giriş yapmamış
-  header("location: login.php");
-  die();
-}
--- db2/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-@session_start(); // Oturumu aç
-@session_destroy(); // Oturumu sonlandır
-?>
-<h1>Oturum sonlandı.</h1>
-<p><a href='login.php'>Yeniden giriş yap...</a></p>
